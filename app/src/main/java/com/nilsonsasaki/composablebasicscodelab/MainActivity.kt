96f8d3447523e6e3eb57e07252e58370486d18a8
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-17 14:35:46 -0300

    added spring animation on extra padding

--- app/src/main/java/com/nilsonsasaki/composablebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/composablebasicscodelab/MainActivity.kt
@@ -3,6 +3,9 @@ package com.nilsonsasaki.composablebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -78,7 +81,9 @@ fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(if (expanded.value) 48.dp else 0.dp,
+    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy,
+    stiffness = Spring.StiffnessLow))
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -88,7 +93,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
